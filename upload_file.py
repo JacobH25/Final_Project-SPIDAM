2ac64c502018e402d833e77dc2707ad80a8d1355
jacob hensley <jrhensley4795@gmail.com>
2024-04-20 13:09:21 -0400

    Added documentation to upload_file

--- upload_file.py
+++ upload_file.py
@@ -1,7 +1,15 @@
+# upload_file.py
+# Programmer Name: Jacob Hensley
+# File Created: 4/20/2024
+
+# import easygui for opening File Explorer
 import easygui as eg
 
 
+# upload_file function
 def upload_file():
+    # Initializes file as file selected by user via File Explorer
     file = eg.fileopenbox()
+    # returns selected file
     return file
 
